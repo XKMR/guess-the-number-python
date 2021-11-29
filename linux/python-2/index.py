efa5c110ee5f1b804abe2b531cdb8d85b3704527
kamyar <58393719+XKMR@users.noreply.github.com>
2021-11-29 15:23:56 +0000

    Add files via upload

---
+++ linux/python-2/index.py
@@ -0,0 +1,27 @@
+import random
+import os
+
+x = 0
+os.system('clear')
+num = random.randint(0,100)
+print "GUESS THE NUMBER GAME!"
+print "I will choose a Number and you should guess it!"
+raw_input('Press Enter To Start!')
+os.system("clear")
+n = int(input('guess: '))
+os.system("clear")
+
+while n != num:
+	if n < num:        
+	 x=x+1
+	 print n,' ==> guess a bigger number  -    ',x,'  Guesse(s)'
+	 n = int(input('guess: '))
+	 os.system("clear")
+	if n > num:      
+	 x=x+1  
+	 print n,' ==> guess a smaller number  -    ',x,'  Guesse(s)'
+	 n = int(input('guess: '))
+	 os.system("clear")
+os.system("clear")
+x=x+1
+print n,' ==> NICE!    number was: ',num,'   -    with ',x,' guesse(s)'
